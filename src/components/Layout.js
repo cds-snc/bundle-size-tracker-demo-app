aec3e91bfc63ba79bbb8a492fc9229a799bacd8e
Max Neuvians <max@neuvians.net>
2018-12-31 10:17:12 -0500

    Added moment (#1)
    * Added momemnt
    
    * Trying local config
    
    * Moved CDS bundle size plugin to dev deps
    
    * Update package.json

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,9 @@
 import React from "react"; // eslint-disable-line
 import isToday from "date-fns/is_today";
+import moment from "moment"
 
 export const Layout = () => {
 
-  return <div>Hello React {isToday(new Date()) ? "yes" : "no"} </div>;
+  return <div>Hello React {isToday(new Date()) ? "yes" : "no"} or {moment().format()}</div>;
 
 };
