8b17d9a90630e28ca942b4ba432125fbd89a502c
Tim Arney <timarney@users.noreply.github.com>
2018-12-31 14:11:51 -0500

    add styled-components (#9)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,21 @@
 import React from "react"; // eslint-disable-line
 import isToday from "date-fns/is_today";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
 
 export const Layout = () => {
-  return <div>Hello React {isToday(new Date()) ? "yes" : "no"}</div>;
+  return (
+    <div>
+      Hello React {isToday(new Date()) ? "yes" : "no"}
+      <Button>Hello</Button>
+    </div>
+  );
 };
