d6df684021b872c1edb5b450319382536986839a
Tim Arney <timarney@users.noreply.github.com>
2018-12-31 10:55:37 -0500

    remove moment (#6)

--- src/client.js
+++ src/client.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Layout } from "./components/Layout";
 const app = document.getElementById("app");
-//ReactDOM.hydrate(<Layout />, app);
 ReactDOM.render(<Layout />, app);
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,6 @@
 import React from "react"; // eslint-disable-line
 import isToday from "date-fns/is_today";
-import moment from "moment"
 
 export const Layout = () => {
-
-  return <div>Hello React {isToday(new Date()) ? "yes" : "no"} or {moment().format()}</div>;
-
+  return <div>Hello React {isToday(new Date()) ? "yes" : "no"}}</div>;
 };
