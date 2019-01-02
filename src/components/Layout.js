14bcea58c765212962d4e06d51267d55609c45b8
Tim Arney <timarney@users.noreply.github.com>
2019-01-02 11:49:11 -0500

    remove date fns (#12)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,9 @@
 import React from "react"; // eslint-disable-line
-import isToday from "date-fns/is_today";
-import styled from "styled-components";
 import { Example } from "./List";
-
-const Main = styled.div`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
-  margin: 1em 1em;
-  padding: 0.25em 1em;
-`;
 
 export const Layout = () => {
   return (
     <div>
-      <Main>Hello {isToday(new Date()) ? "yes" : "no"}</Main>
       <Example />
     </div>
   );
