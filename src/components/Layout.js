6f7ef1c5f81436df1e116f8f2f46094738013274
Tim Arney <timarney@users.noreply.github.com>
2019-01-05 06:11:56 -0500

    Add moment (#13)
    * add moment

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from "react"; // eslint-disable-line
 import { Example } from "./List";
+import moment from "moment";
 
 export const Layout = () => {
   return (
     <div>
       <Example />
+      <div style={{ marginTop: "20px" }}>{moment().format()}</div>
     </div>
   );
 };
