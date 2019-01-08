e34bb7c485f7480735302fbf33c038d2dc100f37
Tim Arney <timarney@users.noreply.github.com>
2019-01-08 12:47:31 -0500

    Swap to CRA / Rewired + add blocking script (#16)
    * Swap to CRA / Rewired + add blocking script
    
    * update package scripts
    
    * update lock file
    
    * update server

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-// index.js
-require("@babel/register")({});
-require = require("esm")(module);
 module.exports = require("./src/server");
---
+++ public/myscript.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+  console.log("nice");
+}, 1000);
+
+var n = 1000;
+
+for (let i = 0; i < n; i++) {
+  console.log(`Iter {$i}`);
+}
